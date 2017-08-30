d61f563c6aae091bc4bc9aa8f96d17103ab6d72e
Wirunpong Jaingamlertwong <it58070126@office365.it.kmitl.ac.th>
2017-08-30 17:42:37 +0700

    next to vc

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -16,9 +16,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func unwindFormSkillVC(unwindSegue: UIStoryboardSegue) {
+        
     }
 
 
