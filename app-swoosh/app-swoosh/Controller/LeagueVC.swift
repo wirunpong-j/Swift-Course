df4bfb8f197aa5180227880e267933b3aa4fe50b
Wirunpong Jaingamlertwong <it58070126@office365.it.kmitl.ac.th>
2017-08-30 18:28:14 +0700

    add IBAction

---
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,44 @@
+//
+//  LeagueVC.swift
+//  app-swoosh
+//
+//  Created by Wirunpong Jaingamlertwong on 8/30/2560 BE.
+//  Copyright © 2560 Wirunpong Jaingamlertwong. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    @IBOutlet weak var nextBtn: BorderButton!
+    
+    var player:Player!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        player = Player()
+    }
+    
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "SkillVCSegue", sender: self)
+    }
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectedLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectedLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectedLeague(leagueType: "coed")
+    }
+    
+    func selectedLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
+}
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Wirunpong Jaingamlertwong on 8/30/2560 BE.
+//  Copyright © 2560 Wirunpong Jaingamlertwong. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
