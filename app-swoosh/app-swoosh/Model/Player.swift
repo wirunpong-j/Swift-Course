aa90d87b5b138d30fa0e2a623c5d49e449ed1485
Wirunpong Jaingamlertwong <it58070126@office365.it.kmitl.ac.th>
2017-08-30 18:36:37 +0700

    send data to another vc

--- app-swoosh/app-swoosh/Model/Player.swift
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct Player {
-    var desiredLeague: String?
-    var selectedSkillLevel: String?
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
     
 }
